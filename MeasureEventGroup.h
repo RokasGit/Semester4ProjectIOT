1206fd42dc7e1f3520e1c37576cc4fbeb1d7c999
rokasgit <ifamous.rp@gmail.com>
2022-05-05 18:36:09 +0200

    Files deleted by accident

---
+++ MeasureEventGroup.h
@@ -0,0 +1,18 @@
+/*
+* MeasureEventGroup.h
+*
+* Created: 5/3/2022 8:15:53 PM
+*  Author: rokas
+*/
+#pragma once
+#include <ATMEGA_FreeRTOS.h>
+#include <event_groups.h>
+
+/* Measure Event Group */
+#define BIT_READY_TO_MEASURE_CO2 (1 << 0)
+#define BIT_READY_TO_MEASURE_TEMPERATURE (1 << 1)
+#define BIT_READY_TO_MEASURE_HUMIDITY (1 << 2)
+// extend the event group visibility
+extern EventGroupHandle_t measureEventGroup;
+
+void measureEventGroup_initilize();
---
+++ SensorDataPackageHandler.h
@@ -0,0 +1,18 @@
+/*
+* SensorDataPackageHandler.h
+*
+* Created: 5/3/2022 7:26:17 PM
+*  Author: rokas
+*/
+#pragma once
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+
+#include <ATMEGA_FreeRTOS.h>
+#include <lora_driver.h>
+
+void sensorDataPackageHandler_setCo2Ppm(uint16_t ppm);
+void sensorDataPackageHandler_setPackageLength(uint8_t packageLength);
+lora_driver_payload_t sensorDataPackageHandler_getLoraPayload(uint8_t port_No);
+
